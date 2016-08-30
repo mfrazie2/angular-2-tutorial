9650fa3bb18d414b76f3f6cdfac0b45b408eeae3
Mike Frazier <michael.s.frazier@gmail.com>
2016-08-30 10:19:40 -0600

    Create video list; add template

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,11 +1,22 @@
 import {Component} from 'angular2/core';
 import {Config} from './config.service';
+import {PlaylistComponent} from './playlist.component';
+import {Video} from './video';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{ mainHeading }}</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
 export class AppComponent {
   mainHeading = Config.MAIN_HEADING;
+  videos: Array<Video>;
+  
+  constructor() {
+    this.videos = [
+      new Video(1, "Installing Django", "qgGIqRFvFFK", "Get Django on your computer, yo!"),
+      new Video(2, "Surviving Wilderness", "Fgwy-UdtyLS", "How to not die in the wild with Dwight Schrute")
+    ];
+  }
 }
